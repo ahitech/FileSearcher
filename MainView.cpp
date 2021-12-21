8536118cde4341df7b68f974f3c2f25aa1808b97
Alex Hitech <ahitech@gmail.com>
2021-12-21 02:01:08 +0200

    Changes in the UI

--- MainView.cpp
+++ MainView.cpp
@@ -5,6 +5,9 @@
 
 #include "MainView.h"
 
+#undef B_TRANSLATION_CONTEXT
+#define B_TRANSLATION_CONTEXT "FileSearcherMainWindow"
+
 
 
 MainView::MainView (BRect frame)
@@ -13,11 +16,38 @@ MainView::MainView (BRect frame)
 			 B_WILL_DRAW | B_NAVIGABLE)
 {
 	this->SetViewColor (ui_color(B_MENU_BACKGROUND_COLOR));
-	
-	m_PathToSearch = new BTextControl(BRect(10, 10), "Path");
-	
-	BuildListOfFilesUI();
-	
+		
+	BLayoutBuilder::Group<>(this, B_VERTICAL, 0)
+		.AddGrid(B_HORIZONTAL, B_USE_ITEM_SPACING)
+			.Add(m_PathToSearchLabel = new BStringView("Path label", 
+											B_TRANSLATE("Path to search:")),
+					0, 0)
+			.Add(m_PathToSearch = new BTextControl(NULL, NULL,
+					new BMessage(MSG_ADD_PATH)),
+					1, 0)
+			.Add(m_ChooseFolder = new BButton(B_TRANSLATE("Add folder to search"),
+											  new BMessage(MSG_ADD_FOLDER)),
+					2, 0)
+			.Add(m_ContainingTextLabel = new BStringView("Containing text label",
+											B_TRANSLATE("Containing text:")),
+					0, 1)
+			.Add(m_ContainingText = new BTextControl(NULL, NULL,
+											new BMessage(MSG_ADD_TEXT)),
+					1, 1)
+			.Add(m_EnableRegEx = new BCheckBox(B_TRANSLATE("Enable Reg Expr"),
+											new BMessage(MSG_EN_REGEXP)),
+					2, 1)
+			.Add(m_FilesNamesCriteriaLabel = new BStringView(
+											"Filter on names label",
+											B_TRANSLATE("Filter on names:")),
+					0, 2)
+			.Add(m_FilesNamesCriteria = new BTextControl(NULL, NULL,
+											new BMessage(MSG_ADD_FILTER)),
+					1, 2, 2, 1)
+			.End()				
+		.AddGlue()
+		.SetInsets(B_USE_WINDOW_SPACING);
+
 	
 }
 
@@ -28,9 +58,10 @@ MainView::~MainView ()
 }
 
 
-void MainView::BuildListOfFilesUI()
+void MainView::BuildFilesCriteriaUI()
 {
-		
+	
+	
 }
 
 void MainView::AttachedToWindow() 
@@ -38,4 +69,10 @@ void MainView::AttachedToWindow()
    if ( Parent() )
       SetViewColor(Parent()->ViewColor());
    BView::AttachedToWindow();
+}
+
+
+void MainView::MessageReceived (BMessage* in)
+{
+	BView::MessageReceived(in);
 }
--- MainView.h
+++ MainView.h
@@ -5,15 +5,28 @@
 #ifndef MAIN_VIEW_H
 #define MAIN_VIEW_H
 
+#include <Button.h>
+#include <Catalog.h>
 #include <CheckBox.h>
+#include <File.h>
+#include <LayoutBuilder.h>
 #include <ListItem.h>
 #include <OutlineListView.h>
 #include <ScrollView.h>
+#include <StringView.h>
 #include <TabView.h>
 #include <TextControl.h>
 #include <View.h>
 
+
 #include <SupportDefs.h>
+
+
+const uint32	MSG_ADD_PATH 		= 'adpt';
+const uint32	MSG_ADD_TEXT		= 'txts';
+const uint32	MSG_EN_REGEXP		= 'enrx';
+const uint32	MSG_ADD_FILTER		= 'fltr';
+const uint32	MSG_ADD_FOLDER		= 'adfl';
 
 
 class MainView :
@@ -24,15 +37,21 @@ public:
 	~MainView ();
 	
 	void AttachedToWindow();
+	void MessageReceived(BMessage* in);
 protected:
+	BTextControl*	m_FilesNamesCriteria;
+
+	BStringView*	m_PathToSearchLabel;
 	BTextControl*	m_PathToSearch;
 	BButton*		m_ChooseFolder;
+	BStringView*	m_ContainingTextLabel;
 	BTextControl*	m_ContainingText;
 	BCheckBox*		m_EnableRegEx;
+	BStringView*	m_FilesNamesCriteriaLabel;
 	BScrollView*	m_ListOfFiles;
 	BTabView*		m_InfoTabs;
 	
-	void	BuildListOfFilesUI();
+	void	BuildFilesCriteriaUI();
 };
 
 
